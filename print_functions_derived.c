8f4b83af30f8130fd060ae5d96188e75c02a95f4
yosef-zewdu <dyaz.almz@gmail.com>
2022-04-19 17:34:16 +0300

    fixing betty error

--- print_functions_derived.c
+++ print_functions_derived.c
@@ -10,12 +10,12 @@
 int print_unsignedToBinary(va_list arg)
 {
 
-  unsigned int n = va_arg(arg, unsigned int);
-  unsigned int printed;
+unsigned int n = va_arg(arg, unsigned int);
+unsigned int printed;
 
-  print_binary(n, &printed);
+print_binary(n, &printed);
 
-  return (printed);
+return (printed);
 }
 
 
